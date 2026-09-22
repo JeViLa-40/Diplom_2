2438ffc90d4c3372ba780ba1bd137124eea84ee9
Anna Pavlovna <istominaannie@yandex.ru>
2026-09-22 19:16:16 +1200

    fix tests, fixture and generators after code review

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -5,14 +5,15 @@ import allure
 class TestCreateUser:
 
     @allure.title('Проверка успешного создания уникального пользователя')
-    def test_create_unique_user_success(self, create_user_with_delete):
-        response = create_user_with_delete['response']
+    def test_create_unique_user_success(self, generate_user_with_delete):
+        body = generate_user_with_delete
+        response = UserMethods.create_user(body)
         assert response.status_code == 200
         assert response.json()["success"] is True
         
     @allure.title('Проверка ошибки создания пользователя, который уже зарегистрирован')
     def test_cannot_create_duplicate_user(self, create_user_with_delete):
-        body = create_user_with_delete['body']
+        body = create_user_with_delete
         response_dublicate = UserMethods.create_user(body)
         assert response_dublicate.status_code == 403
         assert response_dublicate.json()["message"] == 'User already exists'
